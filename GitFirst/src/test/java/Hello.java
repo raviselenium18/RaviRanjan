8d3a9742942fec163f0d89f5e364ba06c60c2de7
Singh <RaSingh@elementcorp.com>
2016-07-21 19:51:03 +0530

    Adding Files

---
+++ GitFirst/src/test/java/Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1. Hello 1st Git!!");
+		System.out.println("2. Hello 1st Git!!");
+	}
+
+}
